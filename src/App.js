42e3f9c9b2020c2dc596d9e4c2dcb19da38278f0
Yichun Li <yichun169@gmail.com>
2021-11-06 20:23:07 -0700

    Add AuthButton component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
+import AuthButton from "./components/AuthButton";
+import Habits from "./components/Habits";
 import MainSection from "./components/MainSection";
 import SideSection from "./components/SideSection";
 import { TitleLogo } from "./components/TitleLogo";
 
 function App() {
   return (
-    <div className="h-screen flex ">
+    <div className="h-screen flex">
       <SideSection>
         <TitleLogo />
         <Habits />
       </SideSection>
       <MainSection />
+
+      {/* <AuthButton text="sign up"/>
+      <AuthButton text="log in"/> */}
+
     </div>
   );
 }
---
+++ src/components/AuthButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AuthButton = ({text}) => {
+    return (
+        <button onClick={()=>console.log("clicked")}>{
+            <div className="p-1 rounded-full bg-gray-200 hover:bg-gray-300 bg-opacity-50 text-center h-8 w-20" >
+                {text}
+            </div>
+        }</button>
+    );
+};
+
+export default AuthButton
