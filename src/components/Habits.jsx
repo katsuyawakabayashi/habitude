9cf31a992793ae27a1a706d424eab988ff43059d
Katsuya Wakabayashi <wakabayashi@ucla.edu>
2021-11-10 22:20:04 -0800

    Rename PledgeHabitContent to NewHabitContent
    Remove PledgeHabit
    Add + button to add content in Habits
    Introduce 'New' prefix instead of pledge

--- src/components/Habits.jsx
+++ src/components/Habits.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Habit from "./Habit";
-import PledgeHabit from "./PledgeHabit";
 
 //need to rename Habits to HabitList
 const HabitPanel = () => {
@@ -8,8 +7,9 @@ const HabitPanel = () => {
     <>
       <div className="flex justify-center items-center">
         <div className="flex-auto text-2xl ">my habits</div>
-
-        <PledgeHabit />
+        <button className="flex-auto" onClick={() => console.log("Add habit")}>
+          +
+        </button>
       </div>
       <Habit />
       <div className="text-lg">{">"} Sample Habit</div>
---
+++ src/components/NewHabitContent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewHabitContent = ({}) => {
+  return <div></div>;
+};
+
+export default NewHabitContent;
