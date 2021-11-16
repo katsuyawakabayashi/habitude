c76aec06e3b1e95e27c1d98dda26fcb77ba429fd
Yichun Li <yichun169@gmail.com>
2021-11-15 18:07:17 -0800

    Add Search bar, display in HabitList

--- src/components/Habit.jsx
+++ src/components/Habit.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const Habit = ({ habitName, handleMainSection }) => {
   return (
     <button onClick={() => handleMainSection(habitName)}>
-      <div className="text-sm bg-gray-600 bg-opacity-10 p-1 m-2 rounded-lg">
+      <div className="text-sm bg-gray-600 bg-opacity-10 hover:bg-gray-400 p-1 m-2 rounded-lg">
         {habitName}
       </div>
     </button>
--- src/components/HabitPanel.jsx
+++ src/components/HabitPanel.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import HabitList from "./HabitList";
+import SearchBar from "./SearchBar";
 
 const HabitPanel = ({ mainSection, setMainSection }) => {
   const handleMainSection = (e) => {
