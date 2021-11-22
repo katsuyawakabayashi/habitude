92a9731e2169fd8e6b7cf467c7e991365e262c81
Brad <bradlowe@g.ucla.edu>
2021-11-21 16:26:12 -0800

    Update HabitContent page

--- src/components/HabitContent.jsx
+++ src/components/HabitContent.jsx
@@ -5,13 +5,17 @@ const HabitContent = ({ habitName }) => {
   return (
     <div>
       <div className="flex flex-row space-x-10">
-        <div>
+        <div className="space-y-16">
+          <div className="space-y-4">
             <div>today is: SAMPLE DATE</div>
-            <div>{habitName}</div>
+            <div className="text-3xl">{habitName}</div>
+          </div>
+          <div className="space-y-4">
             <div>longest streak: 25 days</div>
             <div>start date: 1/11/11</div>
             <div>notes:</div>
-            <input className="bg-gray-200"></input>
+            <textarea className="bg-gray-200 h-40"></textarea>
+          </div>
         </div>
         <div className="flex flex-col space-y-5">
           <div><HabitHeatmap /></div>
