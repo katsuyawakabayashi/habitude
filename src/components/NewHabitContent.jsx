e936307d2bc5192d8551cf2cb46e8d23556dae4e
Brad <bradlowe@g.ucla.edu>
2021-11-23 01:18:52 -0800

    Add dark mode to HabitContent

--- src/components/NewHabitContent.jsx
+++ src/components/NewHabitContent.jsx
@@ -4,7 +4,7 @@ const NewHabitContent = ({}) => {
   return (
     <div className="w-screen bg-gray-100 dark:bg-gray-800 dark:text-gray-300">
       <div className="flex flex-col">
-        Everyday, I will:<input className="bg-gray-200 dark:bg-gray-300 w-40"></input>
+        Everyday, I will:<input className="bg-gray-300 w-40"></input>
         <button className="w-40"
           onClick={() => {
             console.log("API request here");
