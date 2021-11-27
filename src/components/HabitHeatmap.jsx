e929ee3fcb2dfd8ec3b2406275f44337b8e3e305
megys <meganyschmid@gmail.com>
2021-11-27 02:25:28 -0800

    Initialize first month of data to false
    When creating a new habit, the month of January for the current year
    will have associated values for completed all set to false

--- src/components/HabitHeatmap.jsx
+++ src/components/HabitHeatmap.jsx
@@ -17,7 +17,6 @@ const writeDate = ( dateString ) => {
 const HabitHeatmap = ({ setCurrentDate, habitData }) => {
     return (
         <div className="p-5 pb-0 flex flex-row space-x-4 justify-items-end">
-        <div>
         <CalendarHeatmap
             startDate={new Date('2020-12-31')}
             endDate={new Date('2021-12-31')}
@@ -50,19 +49,6 @@ const HabitHeatmap = ({ setCurrentDate, habitData }) => {
             }} 
         />
         <ReactTooltip multiline={true}/>
-        </div>
-        {/* <div className="flex flex-col space-y-2">
-            
-              <button className="text-base font-semibold rounded-lg py-2 px-4 bg-green-600 text-white">
-                2021
-              </button>
-              <button className="text-base font-medium rounded-lg py-2 px-4 bg-white text-black">
-                2020
-              </button>
-              <button className="text-base font-medium rounded-lg py-2 px-4 bg-white text-black">
-                2019
-              </button>
-            </div> */}
         </div>
     );
 };
