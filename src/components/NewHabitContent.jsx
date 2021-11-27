7a6e56fc9217ffe56e97e9136b59fe711063bb98
Johanna <jojo.cj.liu@gmail.com>
2021-11-26 19:18:12 -0800

    Connect to firebase with sendHabitToFirestore function
    Use setDoc in firebase.js and make a new function
    Set function in button in NewHabitContent.jsx

--- src/components/NewHabitContent.jsx
+++ src/components/NewHabitContent.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { sendHabitToFirestore } from "../firebase";
+import { useAuth } from "../firebase";
 
 const NewHabitContent = ({}) => {
+  const currentUser = useAuth();
+  if(currentUser) {
+    console.log('uid: ', currentUser.uid)
+    const currentUserPath=currentUser.uid;
+  }
+
   return (
     <div className="w-screen bg-gray-100 dark:bg-gray-800 dark:text-gray-300">
       <div className="flex flex-col m-5 space-y-4">
@@ -10,6 +18,9 @@ const NewHabitContent = ({}) => {
         <button className="w-40 p-2 rounded-lg bg-green-600 dark:text-gray-300 text-white"
           onClick={() => {
             console.log("API request here");
+            if(currentUser) {
+              sendHabitToFirestore(currentUser.uid, "habit was sent");
+            }
           }}
         >
           Add a habit
