7d605579a5a2b4b42c19c016207b3ae7c08e4a20
megys <meganyschmid@gmail.com>
2021-11-27 21:34:32 -0800

    Fix design flaws
    Make components closer together
    Make size of "track your progress" bar not dependent on
    length of quote

--- src/components/CheckoffPanel.jsx
+++ src/components/CheckoffPanel.jsx
@@ -3,7 +3,7 @@ import CheckoffList from "./CheckoffList";
 
 const CheckoffPanel = () => {
   return (
-    <div>
+    <div className="max-w-xl">
       <div className="p-4 rounded-lg bg-green-600 dark:text-gray-300 text-white">
         <div className="font-serif text-2xl font-medium">
           Track your progress
