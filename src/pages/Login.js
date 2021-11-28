d81780bbcf3f340b65f7101c128a073fb5ff69a3
Johanna <jojo.cj.liu@gmail.com>
2021-11-28 15:18:37 -0800

    Add logout button on login and signup pages
    Add login function in firebase.js
    Add handleLogin function and button in Login.js and Signup.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,13 +3,13 @@ import AuthButton from "../components/AuthButton";
 import AuthContainer from "../components/AuthContainer";
 import AuthInput from "../components/AuthInput";
 import { useRef, useState } from "react";
-import { login, useAuth } from "../firebase"
+import { login, useAuth, logout } from "../firebase"
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
-
+ 
   const [ loading, setLoading ] = useState(false);
   const currentUser = useAuth();
 
@@ -23,6 +23,16 @@ const Login = () => {
     setLoading(false);
   }
 
+  async function handleLogout(){
+    setLoading(true);
+    try {
+      await logout();
+    } catch {
+      alert("Error! Couldn't logout");
+    }
+    setLoading(false);
+  }
+
   return (
     <AuthContainer>
       Currently logged in as: { currentUser?.email }
@@ -30,9 +40,11 @@ const Login = () => {
       <AuthInput ref={passwordRef} type="password" placeholder="password" />
       <div className="flex justify-center text-lg text-gray-500 pt-2 pb-2">
       <button disabled={loading} onClick={handleLogin}
-        className="px-3 py-1 rounded-full bg-gray-200 hover:bg-gray-300 bg-opacity-50"
-      >
+        className="px-3 py-1 rounded-full bg-gray-200 hover:bg-gray-300 bg-opacity-50">
         log in
+      </button>
+      <button onClick={handleLogout} className="px-3 py-1 rounded-full bg-gray-200 hover:bg-gray-300 bg-opacity-50">
+        Log out
       </button>
       </div>
       <div className="flex justify-center pb-3">
