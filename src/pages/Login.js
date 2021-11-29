4866a25f9f3c4f096adf31ccefbe6cf9659e91c1
Yichun Li <yichun169@gmail.com>
2021-11-28 17:26:42 -0800

    Update login/signup frontend
    Remove "Log out" buttons from login and signup pages. Fix padding in AuthContainer

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -35,16 +35,12 @@ const Login = () => {
 
   return (
     <AuthContainer>
-      Currently logged in as: { currentUser?.email }
       <AuthInput ref={emailRef} placeholder="email" />
       <AuthInput ref={passwordRef} type="password" placeholder="password" />
       <div className="flex justify-center text-lg text-gray-500 pt-2 pb-2">
       <button disabled={loading} onClick={handleLogin}
         className="px-3 py-1 rounded-full bg-gray-200 hover:bg-gray-300 bg-opacity-50">
         log in
-      </button>
-      <button onClick={handleLogout} className="px-3 py-1 rounded-full bg-gray-200 hover:bg-gray-300 bg-opacity-50">
-        Log out
       </button>
       </div>
       <div className="flex justify-center pb-3">
