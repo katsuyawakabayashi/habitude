038adacf810046467a58bd981d8cc0efb19b7877
Katsuya Wakabayashi <wakabayashi@ucla.edu>
2021-11-28 17:01:48 -0800

    Add auth redirecting

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,63 @@
+import React, { useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router";
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export const AuthContext = React.createContext(null);
+
+const AuthContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [currentUser, setCurrentUser] = useState("default");
+  const [loading, setLoading] = useState(true);
+  const auth = getAuth();
+
+  onAuthStateChanged(auth, async (user) => {
+    await setCurrentUser(user);
+    if (currentUser === null) {
+      setCurrentUser(user);
+    } else if (currentUser === "default") {
+      if (window.location.pathname === "/") {
+        console.log("login now as", currentUser);
+        navigate("/login");
+      }
+
+      console.log("---login now as", currentUser);
+    } else {
+      if (window.location.pathname !== "/") {
+        console.log("login now as", currentUser);
+        navigate("/");
+      }
+      //   if (window.location.pathname === "/") {
+      //     console.log("you just logged out dude", currentUser);
+      //     navigate("/login");
+      //   }
+
+      //navigate("/login");
+    }
+  });
+
+  useEffect(() => {}, []);
+
+  const value = {
+    currentUser,
+    // signup,
+    // login,
+    // logout,
+    // resetPassword,
+    // updateEmail,
+    // updatePassword,
+    // history,
+    // updateUsername,
+    // updateTheme,
+    // userDB,
+    // deleteUser,
+  };
+  return (
+    <AuthContext.Provider value={value}>
+      {/* {!loading && children} */}
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
